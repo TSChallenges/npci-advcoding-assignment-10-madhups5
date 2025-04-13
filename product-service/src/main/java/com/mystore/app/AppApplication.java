651c085eaa889dbd658c4da72452d12861565a5a
madhups5 <paidasubramanyammadhu5@gmail.com>
2025-04-13 15:06:03 +0530

    Completed Assignment-10

--- product-service/src/main/java/com/mystore/app/AppApplication.java
+++ product-service/src/main/java/com/mystore/app/AppApplication.java
@@ -2,6 +2,7 @@ package com.mystore.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
 
 @SpringBootApplication
 public class AppApplication {
